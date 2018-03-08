0b4acc8e8d54a78a7da7987b6dff53053dfec263
Jose <josemedinaruiz97@gmail.com>
2018-03-08 04:13:11 +0100

    pc

--- ProyectoiOS/vista/EmptySegue.swift
+++ ProyectoiOS/vista/EmptySegue.swift
@@ -13,15 +13,5 @@ class EmptySegue: UIStoryboardSegue{
     override func perform() {
     
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
